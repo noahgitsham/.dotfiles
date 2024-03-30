aca07f6144c61d626944b28d41241822f3e83c86
Noah Gitsham <nhgitsham@gmail.com>
2024-03-30 17:00:55 +0000

    Update

--- scripts/scripts/hyposition.sh
+++ scripts/scripts/hyposition.sh
@@ -43,5 +43,9 @@ case "$1" in
 		;;
 esac
 
+# Reset wallpaper
+killall swaybg
+~/scripts/headless.sh ~/scripts/wallpaper.sh
+
 monitor=eDP-1,1920x1200@60,1080x720,1
 monitor=HDMI-A-1,1920x1080,0x0,1,transform,1
